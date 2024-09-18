e56ae9d5499621de68079e60b3aa2c6b9ce417c1
ashish77uab <apatel@gammastack.com>
2024-09-18 11:04:35 +0530

    style: mobile view desing improves

--- client/src/components/cards/ProductCardSkeleton.js
+++ client/src/components/cards/ProductCardSkeleton.js
@@ -3,7 +3,7 @@ import React from "react";
 const ProductCardSkeleton = ({ product }) => {
   return (
     <div>
-      <div className="h-60 w-full relative overflow-hidden rounded-t-lg skeleton mb-1"></div>
+      <div className="md:h-60 h-[120px] w-full relative overflow-hidden rounded-t-lg skeleton mb-1"></div>
       <div className="py-4 px-4 space-y-2 bg-zinc-50">
         <div className="text-center space-y-1">
           <div className=" skeleton h-8 rounded-sm"></div>
--- client/src/components/cards/SubCategoryCardSkeleton.js
+++ client/src/components/cards/SubCategoryCardSkeleton.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SubCategoryCardSkeleton = () => {
   return (
     <div className="shadow-card border-c rounded-lg">
-    <div className="h-60 w-full relative overflow-hidden rounded-t-lg skeleton ">
+      <div className="md:h-60 h-[120px] w-full relative overflow-hidden rounded-t-lg skeleton ">
      
     </div>
     <div className="my-4 text-center bg-gray-50 px-4 ">
